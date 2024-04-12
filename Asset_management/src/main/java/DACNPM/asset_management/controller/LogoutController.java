6cf5c3756cd79c2bfdc30b6e8237f9baa650a1d2
nhi2527 <nhi2527@gmail.com>
2024-04-12 13:50:38 +0700

    kiểm tra session ở index, add, delete, update

--- Asset_management/src/main/java/DACNPM/asset_management/controller/LogoutController.java
+++ Asset_management/src/main/java/DACNPM/asset_management/controller/LogoutController.java
@@ -1,10 +1,6 @@
 package DACNPM.asset_management.controller;
 
-import DACNPM.asset_management.model.Account;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LogoutController {
