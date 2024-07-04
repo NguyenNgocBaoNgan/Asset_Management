44d2c2620eaaa8bd89f098be93eea8443be8e05d
Nguyễn Ngọc Bảo Ngân <114355473+NguyenNgocBaoNgan@users.noreply.github.com>
2024-07-04 23:16:57 +0700

    giải mã login

--- src/main/java/DACNPM/asset_management/service/SignInService.java
+++ src/main/java/DACNPM/asset_management/service/SignInService.java
@@ -3,16 +3,31 @@ package DACNPM.asset_management.service;
 import DACNPM.asset_management.model.Account;
 import DACNPM.asset_management.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SignInService {
 
     @Autowired
-    AccountRepository accountRepository;
+    private AccountRepository accountRepository;
 
-    public Account checkLogin(int id, String pass) {
-        return accountRepository.checkLogin(id, pass);
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public Account checkLogin(int id, String rawPassword) {
+        Account account = accountRepository.checkLogin(id);
+
+        if (account != null) {
+            // Kiểm tra nếu mật khẩu đã được mã hóa
+            if (passwordEncoder.matches(rawPassword, account.getPassword())) {
+                return account;
+            }
+            // Kiểm tra nếu mật khẩu chưa được mã hóa
+            if (account.getPassword().equals(rawPassword)) {
+                return account;
+            }
+        }
+        return null;
     }
-
 }
